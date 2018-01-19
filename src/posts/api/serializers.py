be9d0fd5dae73024004a61331fc0af4cbd807e3e
kwonheejeong <gmlwjd9405@naver.com>
2018-01-19 15:10:14 +0900

    Apply serializer method field

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import (
+    ModelSerializer, HyperlinkedIdentityField, SerializerMethodField
+)
 
 from posts.models import Post
 
@@ -26,11 +28,13 @@ class PostListSerializer(ModelSerializer):
         view_name='posts-api:delete',
         lookup_field='slug'
     )
+    user = SerializerMethodField()
 
     class Meta:
         model = Post
         field = [
             'url',
+            'user',
             'title',
             'content',
             'publish',
@@ -38,9 +42,15 @@ class PostListSerializer(ModelSerializer):
 
         ]
 
+    def get_user(self, obj):
+        return str(obj.user.username)
+
 
 class PostDetailSerializer(ModelSerializer):
     url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+    markdown = SerializerMethodField()
 
     class Meta:
         model = Post
@@ -51,10 +61,24 @@ class PostDetailSerializer(ModelSerializer):
             'title',
             'slug',
             'content',
-            'publish'
+            'publish',
+            'image',
+            'markdown'
 
         ]
 
+    def get_markdown(self, obj):
+        return obj.get_markdown()
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            return None
+        return image
 
 """
 from posts.models import Post
