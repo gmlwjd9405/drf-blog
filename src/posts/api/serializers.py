af63a2b84545708f2e1d520f1e208997f5876b72
kwonheejeong <gmlwjd9405@naver.com>
2018-01-19 14:08:03 +0900

    Add hyperlinked identity field for URL

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
 
 from posts.models import Post
 
@@ -14,23 +14,38 @@ class PostCreateUpdateSerializer(ModelSerializer):
 
         ]
 
+post_detail_url = HyperlinkedIdentityField(
+    view_name='posts-api:detail',
+    lookup_field='slug'
+)
+
 
 class PostListSerializer(ModelSerializer):
+    url = post_detail_url
+    delete_url = HyperlinkedIdentityField(
+        view_name='posts-api:delete',
+        lookup_field='slug'
+    )
+
     class Meta:
         model = Post
         field = [
+            'url',
             'title',
-            'slug',
             'content',
-            'publish'
+            'publish',
+            'delete_url'
 
         ]
 
 
 class PostDetailSerializer(ModelSerializer):
+    url = post_detail_url
+
     class Meta:
         model = Post
         field = [
+            'url',
             'user',
             'id',
             'title',
