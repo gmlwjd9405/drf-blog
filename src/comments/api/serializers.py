e568ae4fb5ab5151d33e707fbf5c1e3f9d156376
kwonheejeong <gmlwjd9405@naver.com>
2018-01-22 00:45:05 +0900

    Add comment create serializer function part3 (lecture 21)

--- src/comments/api/serializers.py
+++ src/comments/api/serializers.py
@@ -9,13 +9,12 @@ from comments.models import Comment
 User = get_user_model()
 
 
-def create_comment_serializer(model_type='post', slug=None, parent_id=None):
+def create_comment_serializer(model_type='post', slug=None, parent_id=None, user=None):
     class CommentCreateSerializer(ModelSerializer):
         class Meta:
-            model =Comment
+            model = Comment
             field = [
                 'id',
-                'parent',
                 'content',
                 'timestamp',
             ]
@@ -24,32 +23,35 @@ def create_comment_serializer(model_type='post', slug=None, parent_id=None):
             self.model_type = model_type
             self.slug = slug
             self.parent_obj = None
-            if self.parent_id:
+            if parent_id:
                 parent_qs = Comment.objects.filter(id=parent_id)
-                if parent_qs.exists() and parent_qs.count()==1:
+                if parent_qs.exists() and parent_qs.count() == 1:
                     self.parent_obj = parent_qs.first()
-            return super(CommentChildSerializer, self).__init__(*args, **kwargs)
+            return super(CommentCreateSerializer, self).__init__(*args, **kwargs)
 
         def validate(self, data):
             model_type = self.model_type
             model_qs = ContentType.objects.filter(model=model_type)
-            if not model_qs.exists() or model_qs.count()!=1:
+            if not model_qs.exists() or model_qs.count() != 1:
                 raise ValidationError("This is not a valid content type")
             SomeModel = model_qs.first().model_class()
             obj_qs = SomeModel.objects.filter(slug=self.slug)
             # obj_qs = Post.objects.fileter(slug=self.slug)
-            if not obj_qs.exists() or obj_qs.count()!=1:
+            if not obj_qs.exists() or obj_qs.count() != 1:
                 raise ValidationError("this is not a slug for this content type")
             return data
 
         def create(self, validated_data):
             content = validated_data.get("content")
-            user = User.objects.all().first()
+            if user:
+                main_user = user
+            else:
+                main_user = User.objects.all().first()
             model_type = self.model_type
             slug = self.slug
             parent_obj = self.parent_obj
             comment = Comment.objects.create_by_model_type(
-                model_type, slug, user, content,
+                model_type, slug, main_user, content,
                 parent_obj=parent_obj,
             )
             return comment
--- src/comments/api/urls.py
+++ src/comments/api/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .views import (
+    CommentCreateAPIView,
     CommentListAPIView,
-    CommentDetailAPIView
+    CommentDetailAPIView,
     )
 
 urlpatterns = [
     url(r'^$', CommentListAPIView.as_view(), name='list'),
+    url(r'^create/$', CommentCreateAPIView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', CommentDetailAPIView.as_view(), name='thread'),
     # url(r'^(?P<id>\d+)/delete/$', comment_delete, name='delete'),
 ]
